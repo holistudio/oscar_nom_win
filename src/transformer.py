aabc08ab330f9a29cebbb8fa27944de620cb9382
holistudio <daniel.bin.lu@gmail.com>
2026-01-15 18:43:22 -0500

    Basic transformer init

--- src/transformer.py
+++ src/transformer.py
@@ -1,6 +1,26 @@
 import torch
 import torch.nn as nn
 
-class OscarTransformer(nn.Module):
-    def __init__(self, config):
+config = {}
+
+# TODO: positional encoder and think carefully where it is placed.
+class PositionalEncoder(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+class OscarNomTransformer(nn.Module):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.token_emb = nn.Embedding(config['vocab_size'], config['embedding_dim'])
+        self.pos_enc = PositionalEncoder()
+        self.transformer = nn.Transformer(d_model=config['d_model'],
+                                          nhead=config['nhead'],
+                                          num_encoder_layers=config['num_encoder_layers'],
+                                          num_decoder_layers=config['num_decoder_layers'],
+                                          dim_feedforward=config['dim_feedforward'],
+                                          dropout=config['dropout'],
+                                          batch_first=True)
+        self.classification_head = nn.Linear(config['d_model'], 2)
         pass
