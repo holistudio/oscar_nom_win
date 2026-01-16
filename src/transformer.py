ee99890ed8528ac9348833b86d99b7ab7a7d6359
holistudio <daniel.bin.lu@gmail.com>
2026-01-15 20:24:23 -0500

    Update transformer.py

--- src/transformer.py
+++ src/transformer.py
@@ -12,9 +12,11 @@ class PositionalEncoder(nn.Module):
 class OscarNomTransformer(nn.Module):
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        self.chunk_size = config['chunk_size']
         self.token_emb = nn.Embedding(config['vocab_size'], config['embedding_dim'])
         self.pos_enc = PositionalEncoder()
+        
+        # TODO: re-define as separate encoder and decoder pieces
         self.transformer = nn.Transformer(d_model=config['d_model'],
                                           nhead=config['nhead'],
                                           num_encoder_layers=config['num_encoder_layers'],
